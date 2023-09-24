bf48e770f9a5165843cd34ff360329fcda8164af
Zhen-hao <zhenhaolee@gmail.com>
2023-09-24 09:32:29 +0200

    fix bug in encoders for cats NonEmptyList

--- avro4s-cats/src/test/scala/com/sksamuel/avro4s/cats/CatsTest.scala
+++ avro4s-cats/src/test/scala/com/sksamuel/avro4s/cats/CatsTest.scala
@@ -1,5 +1,6 @@
 package com.sksamuel.avro4s.cats
 
+import cats.data.{NonEmptyList, Validated, ValidatedNel}
 import com.sksamuel.avro4s.AvroSchema
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -8,15 +9,15 @@ import com.sksamuel.avro4s.{SchemaFor, Encoder, Decoder}
 class CatsTest extends AnyWordSpec with Matchers {
 
   "avro4s-cats" should {
-    "generate schema for cats.data.Validated" in {
-      case class Test(maybeValid: cats.data.Validated[String, Int])
+    "encode and decode cats.data.Validated" in {
+      case class Test(maybeValid: Validated[String, Int])
       val shcema = summon[SchemaFor[Test]].schema
       println(s"schema:\n${shcema.toString(true)}")
 
       val encoder = summon[Encoder[Test]]
       val decoder = summon[Decoder[Test]]
 
-      val validExample = Test(cats.data.Validated.Valid(1))
+      val validExample = Test(Validated.Valid(1))
       val encodedValidExample = encoder.encode(shcema)(validExample)
       
       println(s"encodedValidExample: $encodedValidExample")
@@ -25,7 +26,9 @@ class CatsTest extends AnyWordSpec with Matchers {
 
       println(s"decodedValidExample: $decodedValidExample")
 
-      val invalidExample = Test(cats.data.Validated.Invalid("error"))
+      decodedValidExample should equal(validExample)
+
+      val invalidExample = Test(Validated.Invalid("error"))
       val encodedInvalidExample = encoder.encode(shcema)(invalidExample)
 
       println(s"encodedInvalidExample: $encodedInvalidExample")
@@ -33,6 +36,42 @@ class CatsTest extends AnyWordSpec with Matchers {
       val decodedInvalidExample = decoder.decode(shcema)(encodedInvalidExample)
 
       println(s"decodedInvalidExample: $decodedInvalidExample")
+
+      decodedInvalidExample should equal(invalidExample)
+
+    }
+
+    "encode and decode cats.data.ValidatedNel" in {
+      // case class Test(maybeValid: ValidatedNel[String, Int])
+      case class CatsValidatedNelTest(errorsOrNum: ValidatedNel[String, Int])
+
+      val shcema = summon[SchemaFor[CatsValidatedNelTest]].schema
+      println(s"schema:\n${shcema.toString(true)}")
+
+      val encoder = summon[Encoder[CatsValidatedNelTest]]
+      val decoder = summon[Decoder[CatsValidatedNelTest]]
+
+      val validExample = CatsValidatedNelTest(Validated.Valid(1))
+      val encodedValidExample = encoder.encode(shcema)(validExample)
+      
+      println(s"encodedValidExample: $encodedValidExample")
+      
+      val decodedValidExample = decoder.decode(shcema)(encodedValidExample)
+
+      println(s"decodedValidExample: $decodedValidExample")
+
+      decodedValidExample should equal(validExample)
+
+      val invalidExample = CatsValidatedNelTest(Validated.Invalid(NonEmptyList.one("error1")))
+      val encodedInvalidExample = encoder.encode(shcema)(invalidExample)
+
+      println(s"encodedInvalidExample: $encodedInvalidExample")
+
+      val decodedInvalidExample = decoder.decode(shcema)(encodedInvalidExample)
+
+      println(s"decodedInvalidExample: $decodedInvalidExample")
+
+      decodedInvalidExample should equal(invalidExample)
 
     }
   }
