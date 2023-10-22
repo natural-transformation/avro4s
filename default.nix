e33c7904d5584b3acd21627e4038fd34d313e6eb
Zhen-hao <zhenhaolee@gmail.com>
2023-10-22 20:26:13 +0200

    update nix files

--- default.nix
+++ default.nix
@@ -2,6 +2,16 @@
 # this file originates from SBTix
 { pkgs ? import <nixpkgs> {}
 , cleanSource ? pkgs.lib.cleanSource
+, gitignore ? (let
+                  src = pkgs.fetchFromGitHub { 
+                    owner = "hercules-ci";
+                    repo = "gitignore.nix";
+                    # put the latest commit sha of gitignore Nix library here:
+                    rev = "9e21c80adf67ebcb077d75bd5e7d724d21eeafd6";
+                    # use what nix suggests in the mismatch message here:
+                    sha256 = "sha256:vky6VPK1n1od6vXbqzOXnekrQpTL4hbPAwUhT5J9c9E=";
+                  };
+                in import src { inherit (pkgs) lib; })
 }:
 
 let
@@ -9,7 +19,7 @@ let
 in
   sbtix.buildSbtLibrary {
     name = "avro4s";
-    src = cleanSource ./.;
+    src = cleanSource (gitignore.gitignoreSource ./.);
     #sbtixBuildInputs = pkgs.callPackage ./sbtix-build-inputs.nix {};
     repo = [
       (import ./repo.nix)
